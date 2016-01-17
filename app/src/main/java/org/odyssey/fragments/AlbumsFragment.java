1f96750cb2a7f7f1dfe94be97ff995e5b80486bd
gnome17 <frederik.luetkes@googlemail.com>
2016-01-17 11:35:15 +0100

    add pbs to albumsfragment

--- app/src/main/java/org/odyssey/fragments/AlbumsFragment.java
+++ app/src/main/java/org/odyssey/fragments/AlbumsFragment.java
@@ -3,6 +3,7 @@ package org.odyssey.fragments;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -23,6 +24,8 @@ import org.odyssey.listener.OnAlbumSelectedListener;
 import org.odyssey.listener.OnArtistSelectedListener;
 import org.odyssey.loaders.AlbumLoader;
 import org.odyssey.models.AlbumModel;
+import org.odyssey.models.TrackModel;
+import org.odyssey.playbackservice.PlaybackServiceConnection;
 import org.odyssey.utils.MusicLibraryHelper;
 import org.odyssey.utils.ScrollSpeedListener;
 
@@ -39,6 +42,8 @@ public class AlbumsFragment extends Fragment implements LoaderManager.LoaderCall
 
     // Save the last scroll position to resume there
     private int mLastPosition;
+
+    private PlaybackServiceConnection mServiceConnection;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -89,6 +94,9 @@ public class AlbumsFragment extends Fragment implements LoaderManager.LoaderCall
 
         // Prepare loader ( start new one or reuse old )
         getLoaderManager().initLoader(0, getArguments(), this);
+
+        mServiceConnection = new PlaybackServiceConnection(getActivity().getApplicationContext());
+        mServiceConnection.openConnection();
     }
 
     @Override
@@ -169,16 +177,76 @@ public class AlbumsFragment extends Fragment implements LoaderManager.LoaderCall
 
         switch (item.getItemId()) {
             case R.id.fragment_albums_action_enqueue:
-                Snackbar.make(getActivity().getCurrentFocus(), "add album to playlist: " + info.position, Snackbar.LENGTH_SHORT).show();
+                enqueueAlbum(info.position);
                 return true;
             case R.id.fragment_albums_action_play:
-                Snackbar.make(getActivity().getCurrentFocus(), "play album: " + info.position, Snackbar.LENGTH_SHORT).show();
+                playAlbum(info.position);
                 return true;
             case R.id.fragment_albums_action_showartist:
                 showArtist(info.position);
                 return true;
             default:
                 return super.onContextItemSelected(item);
+        }
+    }
+
+    private void enqueueAlbum(int position) {
+        // identify current album
+
+        AlbumModel clickedAlbum = (AlbumModel) mAlbumsGridViewAdapter.getItem(position);
+        String albumKey = clickedAlbum.getAlbumKey();
+        // get and enqueue albumtracks
+
+        String whereVal[] = { albumKey };
+
+        String where = android.provider.MediaStore.Audio.Media.ALBUM_KEY + "=?";
+
+        String orderBy = android.provider.MediaStore.Audio.Media.TRACK;
+
+        Cursor cursor = getActivity().getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, MusicLibraryHelper.projectionTracks, where, whereVal, orderBy);
+
+        // get all tracks on the current album
+        if (cursor.moveToFirst()) {
+            do {
+                String trackName = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
+                long duration = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION));
+                int number = cursor.getInt(cursor.getColumnIndex(MediaStore.Audio.Media.TRACK));
+                String artistName = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+                String albumName = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
+                String url = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+
+                TrackModel item = new TrackModel(trackName, artistName, albumName, albumKey, duration, number, url);
+
+                // enqueue current track
+                try {
+                    mServiceConnection.getPBS().enqueueTrack(item);
+                } catch (RemoteException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            } while (cursor.moveToNext());
+        }
+    }
+
+    private void playAlbum(int position) {
+        // Remove old tracks
+        try {
+            mServiceConnection.getPBS().clearPlaylist();
+        } catch (RemoteException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+        // get and enqueue albumtracks
+        enqueueAlbum(position);
+
+        // play album
+        try {
+            mServiceConnection.getPBS().jumpTo(0);
+        } catch (RemoteException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 }
