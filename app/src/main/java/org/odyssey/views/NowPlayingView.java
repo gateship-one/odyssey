830fb230e699a212911b7ff4b79b8f51eff0d753
gnome17 <frederik.luetkes@googlemail.com>
2016-01-18 18:25:53 +0100

    add dialog to save the current playlist

--- app/src/main/java/org/odyssey/views/NowPlayingView.java
+++ app/src/main/java/org/odyssey/views/NowPlayingView.java
@@ -1,20 +1,27 @@
 package org.odyssey.views;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.ColorStateList;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
 import android.os.RemoteException;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
+import android.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -177,8 +184,24 @@ public class NowPlayingView extends RelativeLayout implements SeekBar.OnSeekBarC
                     e.printStackTrace();
                 }
                 return true;
+            case R.id.view_nowplaying_action_saveplaylist:
+                SavePlaylistDialog dlg = new SavePlaylistDialog();
+
+                dlg.show(((AppCompatActivity) getContext()).getSupportFragmentManager(), "SavePlaylistDialog");
+                return true;
             default:
                 return false;
+        }
+    }
+
+    public void savePlaylist(String playlistName) {
+
+        // call pbs and save current playlist to mediastore
+        try {
+            mServiceConnection.getPBS().savePlaylist(playlistName);
+        } catch (RemoteException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 
@@ -740,5 +763,32 @@ public class NowPlayingView extends RelativeLayout implements SeekBar.OnSeekBarC
     public interface NowPlayingDragStatusReceiver {
         enum DRAG_STATUS {DRAGGED_UP,DRAGGED_DOWN}
         void onStatusChanged(DRAG_STATUS status);
+    }
+
+    private class SavePlaylistDialog extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+            final EditText editTextPlaylistName = new EditText(getActivity());
+            editTextPlaylistName.setText(R.string.default_playlist_title);
+            builder.setView(editTextPlaylistName);
+
+            builder.setMessage(R.string.dialog_save_playlist).setPositiveButton(R.string.dialog_action_save, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // accept playlist name
+                    String playlistName = editTextPlaylistName.getText().toString();
+                    savePlaylist(playlistName);
+                }
+            }).setNegativeButton(R.string.dialog_action_cancel, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // User cancelled the dialog dont create playlist
+                }
+            });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
     }
 }
