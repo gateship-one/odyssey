1591e4ba4d23f256fc5f275b4d82006f027eb2db
gnome17 <frederik.luetkes@googlemail.com>
2016-07-24 22:01:52 +0200

    Test: add test for NowplayingInformation

---
+++ app/src/androidTest/java/org/odyssey/NowPlayingInformationTest.java
@@ -0,0 +1,81 @@
+package org.odyssey;
+
+import android.os.Parcel;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.odyssey.models.TrackModel;
+import org.odyssey.playbackservice.NowPlayingInformation;
+
+@RunWith(AndroidJUnit4.class)
+public class NowPlayingInformationTest {
+
+    // trackmodel values
+    private final String TEST_TRACKNAME = "Trackname";
+    private final String TEST_TRACKARTISTNAME = "Trackartistname";
+    private final String TEST_TRACKALBUMNAME = "Trackalbumname";
+    private final String TEST_TRACKALBUMKEY = "Trackalbumkey";
+    private final String TEST_TRACKURL = "Trackurl";
+    private final long TEST_TRACKDURATION = 12345678L;
+    private final int TEST_TRACKNUMBER = 12;
+
+    // nowplaying values
+    private final int TEST_PLAYING = 1;
+    private final String TEST_PLAYINGURL = "PlayingURL";
+    private final int TEST_PLAYINGINDEX = 42;
+    private final int TEST_REPEAT = 0;
+    private final int TEST_RANDOM = 1;
+    private final int TEST_PLAYLISTLENGTH = 13;
+
+    private NowPlayingInformation mNowPlayingInformation;
+    private TrackModel mTrackModel;
+
+    @Before
+    public void setUp() {
+        mTrackModel = new TrackModel(TEST_TRACKNAME, TEST_TRACKARTISTNAME, TEST_TRACKALBUMNAME, TEST_TRACKALBUMKEY, TEST_TRACKDURATION, TEST_TRACKNUMBER, TEST_TRACKURL);
+
+        mNowPlayingInformation = new NowPlayingInformation(TEST_PLAYING, TEST_PLAYINGURL, TEST_PLAYINGINDEX, TEST_REPEAT, TEST_RANDOM, TEST_PLAYLISTLENGTH, mTrackModel);
+    }
+
+    @Test
+    public void testCreate() {
+        // Verify that the object is correct.
+        assertThat(mNowPlayingInformation.getPlaying(), is(TEST_PLAYING));
+        assertThat(mNowPlayingInformation.getPlayingURL(), is(TEST_PLAYINGURL));
+        assertThat(mNowPlayingInformation.getPlayingIndex(), is(TEST_PLAYINGINDEX));
+        assertThat(mNowPlayingInformation.getRepeat(), is(TEST_REPEAT));
+        assertThat(mNowPlayingInformation.getRandom(), is(TEST_RANDOM));
+        assertThat(mNowPlayingInformation.getPlaylistLength(), is(TEST_PLAYLISTLENGTH));
+        assertThat(mNowPlayingInformation.getCurrentTrack(), is(mTrackModel));
+    }
+
+    @Test
+    public void testParcelableWriteRead() {
+        // Set up the Parcelable object to send and receive.
+
+        // Write the data.
+        Parcel parcel = Parcel.obtain();
+        mNowPlayingInformation.writeToParcel(parcel, mNowPlayingInformation.describeContents());
+
+        // After you're done with writing, you need to reset the parcel for reading.
+        parcel.setDataPosition(0);
+
+        // Read the data.
+        NowPlayingInformation createdFromParcel = NowPlayingInformation.CREATOR.createFromParcel(parcel);
+
+        // Verify that the received data is correct.
+        assertThat(createdFromParcel.getPlaying(), is(TEST_PLAYING));
+        assertThat(createdFromParcel.getPlayingURL(), is(TEST_PLAYINGURL));
+        assertThat(createdFromParcel.getPlayingIndex(), is(TEST_PLAYINGINDEX));
+        assertThat(createdFromParcel.getRepeat(), is(TEST_REPEAT));
+        assertThat(createdFromParcel.getRandom(), is(TEST_RANDOM));
+        assertThat(createdFromParcel.getPlaylistLength(), is(TEST_PLAYLISTLENGTH));
+        assertThat(createdFromParcel.getCurrentTrack(), is(mTrackModel));
+    }
+}
