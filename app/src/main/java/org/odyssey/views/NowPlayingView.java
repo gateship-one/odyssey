7408337a064d93b8fc250db41c4df41c9288e59f
Hendrik Borghorst <hendrikborghorst@googlemail.com>
2016-07-26 19:32:56 +0200

    Dynamically set the the size of the draghandler of\n the nowplaying view to the height of the actionbar

--- app/src/main/java/org/odyssey/views/NowPlayingView.java
+++ app/src/main/java/org/odyssey/views/NowPlayingView.java
@@ -14,6 +14,7 @@ import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
