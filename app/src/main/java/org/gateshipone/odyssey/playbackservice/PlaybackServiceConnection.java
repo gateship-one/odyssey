6a704439a6d066db76befe7c54be4ecaa2262075
Frederik Lütkes <frederik.luetkes@googlemail.com>
2018-07-01 14:05:31 +0200

    PlaybackServiceConnection: null playbackservice in closeConnection and notify listener
    # Conflicts:
    #	app/src/main/java/org/gateshipone/odyssey/playbackservice/PlaybackServiceConnection.java

--- app/src/main/java/org/gateshipone/odyssey/playbackservice/PlaybackServiceConnection.java
+++ app/src/main/java/org/gateshipone/odyssey/playbackservice/PlaybackServiceConnection.java
@@ -92,8 +92,12 @@ public class PlaybackServiceConnection implements ServiceConnection {
     /**
      * Disconnects the connection by unbinding from the service (not needed anymore)
      */
-    public void closeConnection() {
+    public synchronized void closeConnection() {
         mContext.unbindService(this);
+        mPlaybackService = null;
+        if (mNotifier != null) {
+            mNotifier.onDisconnect();
+        }
     }
 
     public synchronized IOdysseyPlaybackService getPBS() throws RemoteException {
