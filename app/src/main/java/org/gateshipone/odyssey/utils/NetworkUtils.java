579e09ae6d1b31520601e67e3b13359980fc7e67
Frederik Lütkes <frederik.luetkes@googlemail.com>
2020-03-06 21:08:13 +0100

    resolve deprecated API usage for android M and later

--- app/src/main/java/org/gateshipone/odyssey/utils/NetworkUtils.java
+++ app/src/main/java/org/gateshipone/odyssey/utils/NetworkUtils.java
@@ -24,7 +24,10 @@ package org.gateshipone.odyssey.utils;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
+import android.os.Build;
 
 public class NetworkUtils {
 
@@ -36,16 +39,38 @@ public class NetworkUtils {
      * @return true if a download is allowed else false
      */
     public static boolean isDownloadAllowed(final Context context, final boolean onlyUseWifi) {
-        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        final ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
-
-        if (networkInfo == null) {
+        if (cm == null) {
             return false;
-        } else {
-            boolean isWifi = networkInfo.getType() == ConnectivityManager.TYPE_WIFI || networkInfo.getType() == ConnectivityManager.TYPE_ETHERNET;
+        }
 
-            return !(onlyUseWifi && !isWifi);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            final Network activeNetwork = cm.getActiveNetwork();
+
+            if (activeNetwork != null) {
+                final NetworkCapabilities networkCapabilities = cm.getNetworkCapabilities(activeNetwork);
+
+                if (networkCapabilities != null) {
+                    final boolean isWifi = networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET);
+
+                    return !(onlyUseWifi && !isWifi);
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            @SuppressWarnings("deprecation") final NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+
+            if (networkInfo != null) {
+                final boolean isWifi = networkInfo.getType() == ConnectivityManager.TYPE_WIFI || networkInfo.getType() == ConnectivityManager.TYPE_ETHERNET;
+
+                return !(onlyUseWifi && !isWifi);
+            } else {
+                return false;
+            }
         }
     }
 }
